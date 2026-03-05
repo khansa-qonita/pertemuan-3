6bfac0e589eda31915370706ff1d51e3064bff0b
khansa-qonita <khansaqonita420@gmail.com>
2026-03-05 11:24:17 +0700

    menambahkan number of pass

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -25,3 +25,20 @@ void input(){  //prosedur untuk input
         cin >> arr[i];
     }
 }
+
+
+void bubbleSortArray(){
+    int pass = 1;
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }     
+        }
+        pass = pass + 1;
+    }
+}
