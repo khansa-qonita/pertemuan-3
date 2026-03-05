5c972bf0a57f8fd68123d87d6fb4e1caed4e9de5
khansa-qonita <khansaqonita420@gmail.com>
2026-03-05 10:41:34 +0700

    membuat prosedur untuk input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,3 +4,7 @@ using namespace std;
 
 int arr[20]; //deklarasi variable global array a dengan ukuran 20
 int n; //deklarasi variable global n untuk menyimpan banyaknya elemen
+
+void input(){
+    
+}
