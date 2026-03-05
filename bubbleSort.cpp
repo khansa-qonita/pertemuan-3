0cf2810141dfcf27723deafd505b30a38ccd8137
khansa-qonita <khansaqonita420@gmail.com>
2026-03-05 10:58:27 +0700

    menambahkan output ke layar

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -15,4 +15,13 @@ void input(){  //prosedur untuk input
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "masukkan Elemen Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ":";
+        cin >> arr[i];
+    }
 }
